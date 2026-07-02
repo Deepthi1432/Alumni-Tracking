540dac40a56069d4ec19408bc02d47902177150a
Deepthi1432 <reddydeepthi1432@gmail.com>
2026-07-02 18:49:06 +0530

    notifications.js

--- notifications.js
+++ notifications.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Notification = require("../models/Notification");
+const Notification = require("./Notification");
 
 router.get("/", async (req, res) => {
     try {
